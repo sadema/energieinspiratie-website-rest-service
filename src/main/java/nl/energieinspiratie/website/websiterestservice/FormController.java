6f8b7e4fbb4d3016677b3a4fe674bd2fe67b63a3
Sjoerd Adema <sjoerd@kristalsoftware.nl>
2021-10-21 12:35:59 +0200

    [main] added location header in response

--- src/main/java/nl/energieinspiratie/website/websiterestservice/FormController.java
+++ src/main/java/nl/energieinspiratie/website/websiterestservice/FormController.java
@@ -1,21 +1,25 @@
 package nl.energieinspiratie.website.websiterestservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @Slf4j
 @RestController
 public class FormController {
 
     @PostMapping(path = "/contact", consumes = "application/x-www-form-urlencoded;charset=UTF-8")
-    public String getContactFormData(ContactData contactData) {
+    public ResponseEntity<Void> getContactFormData(ContactData contactData) {
         log.info("Naam: {}, Email: {}, Onderwerp: {}, Bericht: {}, Consent: {}",
                 contactData.getName(),
                 contactData.getEmail(),
                 contactData.getSubject(),
                 contactData.getMessage(),
                 contactData.getConsent());
-        return "redirect:https://energieinspiratie.nl/success";
+        return ResponseEntity.status(HttpStatus.OK).location(URI.create("https://energieinspiratie.nl/success")).build();
     }
 }
