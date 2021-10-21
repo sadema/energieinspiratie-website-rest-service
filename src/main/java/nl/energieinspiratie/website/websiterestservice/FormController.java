5d0ca55a92d73c3ff152f944543b6a4b1f0434f8
Sjoerd Adema <sjoerd@kristalsoftware.nl>
2021-10-21 12:24:27 +0200

    [main] return a redirect

--- src/main/java/nl/energieinspiratie/website/websiterestservice/FormController.java
+++ src/main/java/nl/energieinspiratie/website/websiterestservice/FormController.java
@@ -1,24 +1,21 @@
 package nl.energieinspiratie.website.websiterestservice;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.URI;
 
 @Slf4j
 @RestController
 public class FormController {
 
     @PostMapping(path = "/contact", consumes = "application/x-www-form-urlencoded;charset=UTF-8")
-    public ResponseEntity.BodyBuilder getContactFormData(ContactData contactData) {
+    public String getContactFormData(ContactData contactData) {
         log.info("Naam: {}, Email: {}, Onderwerp: {}, Bericht: {}, Consent: {}",
                 contactData.getName(),
                 contactData.getEmail(),
                 contactData.getSubject(),
                 contactData.getMessage(),
                 contactData.getConsent());
-        return ResponseEntity.created(URI.create("https://energieinspiratie.nl/success"));
+        return "redirect:https://energieinspiratie.nl/success";
     }
 }
